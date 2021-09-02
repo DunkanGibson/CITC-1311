d4709ac18408e023acd3d86e00fe897673bc5753
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-09-02 18:50:43 -0400

    Create test2.java

---
+++ Testing/test2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class test2 {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+        
+    System.out.print("test");
+
+    String first = scanner.nextLine();
+    String second = scanner.nextLine();
+    String third = scanner.nextLine();
+    String fourth = scanner.nextLine();
+
+    System.out.println(first); 
+    System.out.println(second); 
+    System.out.println(third); 
+    System.out.println(fourth); 
+
+    String s = null; // (1)
+    int i = null; 
+    }
+
+}
