3e53dfdb8e824bad6fff0b2601574c5c4f7858ab
Dunkan Gibson <DunkanGibson@gmail.com>
2021-09-11 13:29:04 -0400

    Adding the getters and setters for class

--- Testing/BMI.java
+++ Testing/BMI.java
@@ -14,10 +14,55 @@ public class BMI {
     this("Jim", 26, 215, 60);
   }
 
-  BMI(String newName, int newAge, double newWeight, double newHeight){
+  public BMI(String newName, int newAge, double newWeight, double newHeight){
     this.name = newName;
     this.age = newAge;
     this.weight = newWeight;
     this.height = newHeight;
   }
+  public BMI(String newName, double newWeight, double newHeight){
+    this.name = newName;
+    this.age = 20;
+    this.weight = newWeight;
+    this.height = newHeight;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String newName){
+    this.name = newName;
+  }
+
+  public double getAge() {
+    return age;
+  }
+
+  public void setAge(int newAge){
+    this.age = newAge;
+  }
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double newHeight){
+    this.height = newHeight;
+  }
+  public double getWeight() {
+    return weight;
+  }
+
+  public void setWeight(double newWeight){
+    this.height = newWeight;
+  }
+
+  public double getBMI(){
+    return
+  }
+
+  public String getStatus() {
+    return
+  }
+
 }
