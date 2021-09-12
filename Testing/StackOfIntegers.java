6c7138e20c74f3c39d50b88743b78c47020ff1b2
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-09-12 11:04:36 -0400

    Create StackOfIntegers.java

---
+++ Testing/StackOfIntegers.java
@@ -0,0 +1,25 @@
+package Testing;
+
+/**
+ * StackOfIntegers
+ */
+public class StackOfIntegers {
+
+    private int [] elements;
+    private int size;
+
+    public StackOfIntegers(){
+
+    }
+
+    public StackOfIntegers (int capacity){
+
+    }
+
+    public boolean empty() {
+        if(){
+            return false
+        }
+    }
+
+}
