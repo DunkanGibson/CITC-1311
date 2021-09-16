a7f08731ce0867c85f2d3ce7aaded55d23ea21c7
Dunkan Gibson <DunkanGibson@gmail.com>
2021-09-16 13:40:05 -0400

    Update to discussion 2 with loop

--- Disscusion2/Gibson_Dunkan_LargePower.java
+++ Disscusion2/Gibson_Dunkan_LargePower.java
@@ -2,11 +2,18 @@ import java.math.BigInteger;
 
 public class Gibson_Dunkan_LargePower {
     public static void main(String[] args) {
-        System.out.println("50 raised to the power of 70 is " + LargePower(50,70));
+        System.out.println("50 raised to the power of 70 is " + LargePower(50, 70));
     }
 
-    public static BigInteger LargePower (int x, int y) {
-        BigInteger z = BigInteger.valueOf(x);
-        return z.pow(y);
+    public static BigInteger LargePower (long x, long y) {
+
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= y; i++)
+        result = result.multiply(new BigInteger(x +""));
+        return result;
+
+        // BigInteger z = BigInteger.valueOf(x);
+        // return z.pow(y);
     }
 }
+
