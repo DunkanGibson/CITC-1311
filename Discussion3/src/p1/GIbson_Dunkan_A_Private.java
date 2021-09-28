13f2618880dfa9bf8bd965f40d1bb3129760385d
Dunkan Gibson <DunkanGibson@gmail.com>
2021-09-27 22:04:37 -0400

    Commit of discussion post 3

---
+++ Discussion3/src/p1/GIbson_Dunkan_A_Private.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Gibson_Dunkan_A {
+
+public class A {
+
+private	int i;
+private	void m() {
+
+	}
+
+}
+}
---
+++ Discussion3/src/p2/Gibson_Dunkan_B.java
@@ -0,0 +1,12 @@
+package p2;
+import p1.*;
+
+public class B extends A{
+
+	public void m1(String[] args) {
+		System.out.println(i);
+		m();
+	}
+
+}
+
