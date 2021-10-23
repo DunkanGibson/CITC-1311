5372d0ab90e509244dee06b8ba5b90571304641b
ChocolateSocrotes <52463084+ChocolateSocrotes@users.noreply.github.com>
2021-10-23 11:03:46 -0400

    Finished Assignment 5

---
+++ Chapter5/src/Circle.java
@@ -0,0 +1,28 @@
+public class Circle implements FigureGeometry{
+
+    private float radius;
+
+    public Circle(float theRadius){
+
+        radius = theRadius;
+    }
+
+    public float getRadius() {
+
+        return radius;
+    }
+
+    public void setRadius(float theRadius) {
+
+        this.radius = theRadius;
+    }
+
+    public float getArea(){
+
+        return getRadius() * getRadius() * PI;
+    }
+
+    public float getPerimeter(){
+        return getRadius() * 2 * PI;
+    }
+}
---
+++ Chapter5/src/Point.java
@@ -0,0 +1,25 @@
+public class Point {
+    private int width;
+    private int height;
+
+    public Point(int theWidth, int theHeight){
+        width = theWidth;
+        height = theHeight;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int theWidth) {
+        this.width = theWidth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int theHeight) {
+        this.height = theHeight;
+    }
+}
